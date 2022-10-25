d07cb8f5cc095b29554be9c67cb50d08eaaf128c
claudefunston <sjkoza@gmail.com>
2022-10-24 21:04:02 -0400

    Add files via upload

---
+++ minecraft_user_scripts.sh
@@ -0,0 +1,16 @@
+SERVER_URL="https://piston-data.mojang.com/v1/objects/f69c284232d7c7580bd89a5a4931c3581eae1378/server.jar"
+
+mkdir -p ~/backups
+mkdir -p ~/server
+mkdir -p ~/tools
+
+cd ~/tools
+git clone https://github.com/Tiiffi/mcrcon.git
+cd mcrcon
+gcc -std=gnu99 -Wall -Wextra -Wpedantic -Os -s -o mcrcon mcrcon.c
+
+chmod +x mcrcon
+
+wget "$SERVER_URL" -P /opt/minecraft/server
+echo "eula=true" >> /opt/minecraft/server/eula.txt
+
