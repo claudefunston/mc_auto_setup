d739e1f51823155705c8691b8b07d75518e1c6e7
Stephen Koza <Stephen_Koza@mckinsey.com>
2022-10-24 21:41:17 -0400

    fix script name

--- setup_main.sh
+++ setup_main.sh
@@ -7,11 +7,10 @@ sudo useradd -r -m -U -d /opt/minecraft -s /bin/bash minecraft
 
 sudo cp ./minecraft.service /etc/systemd/system/minecraft.service
 
-sudo su -c 'sh ./setup_minecraft.sh' minecraft
+sudo su -c 'sh ./minecraft_user_scripts.sh' minecraft
 
 sudo chown minecraft server.properties
 
 sudo cp server.properties /opt/minecraft/server/
 
-shopt -s expand_aliases
 alias mcrcon="/opt/minecraft/tools/mcrcon/mcrcon -p McRcOnPw"
