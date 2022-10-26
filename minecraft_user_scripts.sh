4dc99f6d5b171536d769001068912bdb58df2ce8
Stephen Koza <Stephen_Koza@mckinsey.com>
2022-10-26 12:04:54 -0400

    fixing if else syntax

--- minecraft_user_scripts.sh
+++ minecraft_user_scripts.sh
@@ -6,22 +6,22 @@ mkdir -p ~/backups
 mkdir -p ~/server
 mkdir -p ~/tools
 
-if[ ! -d ~/tools/mcrcon ] ; then
+if[ ! -d ~/tools/mcrcon ] then
     cd ~/tools
     git clone https://github.com/Tiiffi/mcrcon.git
     cd mcrcon
-    echo "\n\nCompiling mcrcon\n\n"
+    printf "\n\nCompiling mcrcon\n\n"
     gcc -std=gnu99 -Wall -Wextra -Wpedantic -Os -s -o mcrcon mcrcon.c
 
     chmod +x mcrcon
 else
-    echo "\n\nmcrcon already present. Skipping compile\n\n"
+    printf "\n\nmcrcon already present. Skipping compile\n\n"
 fi
 
-if[ ! -f ~/server/server.jar ]
-    "\n\nFetching server.jar and accepting EULA\n\n"
+if[ ! -f ~/server/server.jar ] then
+    printf "\n\nFetching server.jar and accepting EULA\n\n"
     wget "$SERVER_URL" -P /opt/minecraft/server
     echo "eula=true" >> /opt/minecraft/server/eula.txt
 else
-    echo "\n\nMinecraft server already present. Skipping download\n\n"
+    printf "\n\nMinecraft server already present. Skipping download\n\n"
 fi
